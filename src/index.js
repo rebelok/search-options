a58b240bc10deaee0d9166a127751dc1f1b3f717
Andrew Vdovichencko <avdovichencko@therecongroup.com>
2015-07-06 20:53:00 +0300

    note

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const log = console.log.bind(console,'I:');
   const searchOptions = new SearchOptions();
   log(searchOptions.store);
   React.render(<SearchOptionsControl options={searchOptions.store} optionFilterProp="name"/>, document.getElementById('root'));
-
+//todo add @2x images
 })();
